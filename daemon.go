f6b8b782f33cd2d5c5815876c707675cc2f468c5
funkygao <funky.gao@gmail.com>
2013-12-26 23:48:45 +0800

    daemonize

---
+++ daemon.go
@@ -0,0 +1,69 @@
+package golib
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"syscall"
+)
+
+// daemonize current proc
+func Daemonize(chdir, closefd012 bool) int {
+	var ret, ret2 uintptr
+	var err syscall.Errno
+
+	darwin := runtime.GOOS == "darwin"
+
+	if syscall.Getppid() == 1 { // already a daemon
+		return 0
+	}
+
+	// fork off the parent process
+	ret, ret2, err = syscall.RawSyscall(syscall.SYS_FORK, 0, 0, 0)
+	if err != 0 {
+		return -1
+	}
+
+	// failure
+	if ret2 < 0 {
+		os.Exit(-1)
+	}
+
+	// handle exception for darwin
+	if darwin && ret2 == 1 {
+		ret = 0
+	}
+
+	// if we got a good PID, then we call exit the parent process.
+	if ret > 0 {
+		os.Exit(0)
+	}
+
+	/* Change the file mode mask */
+	_ = syscall.Umask(0)
+
+	// create a new SID for the child process
+	s_ret, s_errno := syscall.Setsid()
+	if s_errno != nil {
+		fmt.Fprintf(os.Stderr, "Error: syscall.Setsid errno: %d\n", s_errno)
+	}
+	if s_ret < 0 {
+		return -1
+	}
+
+	if chdir {
+		os.Chdir("/")
+	}
+
+	if closefd012 {
+		f, e := os.OpenFile("/dev/null", os.O_RDWR, 0)
+		if e == nil {
+			fd := f.Fd()
+			syscall.Dup2(int(fd), int(os.Stdin.Fd()))
+			syscall.Dup2(int(fd), int(os.Stdout.Fd()))
+			syscall.Dup2(int(fd), int(os.Stderr.Fd()))
+		}
+	}
+
+	return 0
+}
