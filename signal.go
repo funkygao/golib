dfdc7f3deca3d45268f04b6da2982080e3e7ee4c
funkygao <funky.gao@gmail.com>
2013-12-26 23:41:00 +0800

    reuseable signal handling mechenism

--- signal.go
+++ signal.go
@@ -3,24 +3,23 @@ package golib
 import (
 	"os"
 	"os/signal"
-	"strings"
 	"sync"
-	"syscall"
 )
 
 type signalHandler func(os.Signal)
 
 var signals = struct {
 	sync.Mutex // map in go is not thread/goroutine safe
-	handlers   map[os.Signal]signalHandler
-	C          chan os.Signal
+
+	handlers map[os.Signal]signalHandler
+	ch       chan os.Signal
 }{
 	handlers: make(map[os.Signal]signalHandler),
-	C:        make(chan os.Signal, 20),
+	ch:       make(chan os.Signal, 20),
 }
 
-func trapSignals() {
-	for sig := range signals.C {
+func init() {
+	for sig := range signals.ch {
 		signals.Lock() // map not goroutine safe in golang
 		handler := signals.handlers[sig]
 		signals.Unlock()
@@ -30,34 +29,14 @@ func trapSignals() {
 	}
 }
 
-func registerSignalHandler(sig os.Signal, handler signalHandler) {
+func RegisterSignalHandler(sig os.Signal, handler signalHandler) {
 	signals.Lock()
 	defer signals.Unlock()
 
 	if _, present := signals.handlers[sig]; !present {
 		signals.handlers[sig] = handler
-		signal.Notify(signals.C, sig)
+		signal.Notify(signals.ch, sig)
 	}
 }
 
-func handleIgnore(sig os.Signal) {}
-
-func showWorkers(sig os.Signal) {
-	logger.Printf("workers: %+v\n", allWorkers)
-	logger.Printf("parsers: %+v\n", parser.Parsers())
-}
-
-func handleInterrupt(sig os.Signal) {
-	logger.Printf("got signal %s\n", strings.ToUpper(sig.String()))
-
-	shutdown()
-}
-
-func setupSignals() {
-	go trapSignals()
-
-	//registerSignalHandler(syscall.SIGHUP, handleInterrupt)
-	registerSignalHandler(syscall.SIGINT, handleInterrupt)
-	registerSignalHandler(syscall.SIGTERM, handleInterrupt)
-	registerSignalHandler(syscall.SIGUSR2, showWorkers)
-}
+func IgnoreSignal(sig os.Signal) {}
