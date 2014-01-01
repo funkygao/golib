2186935bf0bf7e11fb4834066d4a2ded7acc393d
funkygao <funky.gao@gmail.com>
2014-01-01 12:27:52 +0800

    uuid

--- all_test.go
+++ all_test.go
@@ -1,1 +1,12 @@
 package golib
+
+import (
+	"github.com/funkygao/assert"
+	"testing"
+)
+
+func TestUUID(t *testing.T) {
+	uuid, _ := UUID()
+	t.Logf("%v\n", uuid)
+	assert.Equal(t, 32, len(uuid))
+}
---
+++ uuid.go
@@ -0,0 +1,21 @@
+package golib
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+// Generate 32 length hex uuid string
+func UUID() (string, error) {
+	uuid := make([]byte, 16)
+	n, err := rand.Read(uuid)
+	if n != len(uuid) || err != nil {
+		return "", err
+	}
+
+	// TODO: verify the two lines implement RFC 4122 correctly
+	uuid[8] = 0x80 // variant bits see page 5
+	uuid[4] = 0x40 // version 4 Pseudo Random, see page 7
+
+	return hex.EncodeToString(uuid), nil
+}
