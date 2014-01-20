a2edc974860a2190211de0fd8370c5e33a02bf8e
funkygao <funky.gao@gmail.com>
2014-01-20 17:29:17 +0800

    rename

--- sortedmap/sortedmap.go
+++ sortedmap/sortedmap.go
@@ -1,37 +1,37 @@
-package sortmap
+package sortedmap
 
 import (
 	"sort"
 )
 
-type sortedMap struct {
+type SortedMap struct {
 	m map[string]int
 	s []string
 }
 
-func NewSortedMap() *sortedMap {
-	this := new(sortedMap)
+func NewSortedMap() *SortedMap {
+	this := new(SortedMap)
 	this.m = make(map[string]int)
 	return this
 }
 
-func (this *sortedMap) Set(key string, val int) {
+func (this *SortedMap) Set(key string, val int) {
 	this.m[key] = val
 }
 
-func (this *sortedMap) Len() int {
+func (this *SortedMap) Len() int {
 	return len(this.m)
 }
 
-func (this *sortedMap) Less(i, j int) bool {
+func (this *SortedMap) Less(i, j int) bool {
 	return this.m[this.s[i]] > this.m[this.s[j]]
 }
 
-func (this *sortedMap) Swap(i, j int) {
+func (this *SortedMap) Swap(i, j int) {
 	this.s[i], this.s[j] = this.s[j], this.s[i]
 }
 
-func (this *sortedMap) SortedKeys() []string {
+func (this *SortedMap) SortedKeys() []string {
 	this.s = make([]string, len(this.m))
 	i := 0
 	for key, _ := range this.m {
--- sortedmap/sortedmap_test.go
+++ sortedmap/sortedmap_test.go
@@ -1,4 +1,4 @@
-package sortmap
+package sortedmap
 
 import (
 	"testing"
