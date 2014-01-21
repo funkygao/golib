9749559e4f5015e6989353f648df23fd8d444365
funkygao <funky.gao@gmail.com>
2014-01-21 10:05:14 +0800

    goroutine safe

--- sortedmap/sortedmap.go
+++ sortedmap/sortedmap.go
@@ -2,9 +2,11 @@ package sortedmap
 
 import (
 	"sort"
+	"sync"
 )
 
 type SortedMap struct {
+	*sync.RWMutex
 	m map[string]int
 	s []string
 }
@@ -12,25 +14,37 @@ type SortedMap struct {
 func NewSortedMap() *SortedMap {
 	this := new(SortedMap)
 	this.m = make(map[string]int)
+	this.RWMutex = new(sync.RWMutex)
 	return this
 }
 
 func (this *SortedMap) Set(key string, val int) {
+	this.Lock()
 	this.m[key] = val
+	this.Unlock()
 }
 
 func (this *SortedMap) Get(key string) int {
+	this.RLock()
+	defer this.RUnlock()
 	return this.m[key]
 }
 
-func (this *SortedMap) Inc(key string) int {
-	v := this.m[key]
-	v += 1
+func (this *SortedMap) Inc(key string, delta int) int {
+	this.Lock()
+	defer this.Unlock()
+	v, present := this.m[key]
+	if !present {
+		return -1
+	}
+	v += delta
 	this.m[key] = v
 	return v
 }
 
 func (this *SortedMap) Len() int {
+	this.RLock()
+	defer this.RUnlock()
 	return len(this.m)
 }
 
@@ -43,6 +57,8 @@ func (this *SortedMap) Swap(i, j int) {
 }
 
 func (this *SortedMap) SortedKeys() []string {
+	this.RLock()
+	defer this.RUnlock()
 	this.s = make([]string, len(this.m))
 	i := 0
 	for key, _ := range this.m {
--- sortedmap/sortedmap_test.go
+++ sortedmap/sortedmap_test.go
@@ -1,13 +1,24 @@
 package sortedmap
 
 import (
+	"github.com/funkygao/assert"
 	"testing"
 )
 
-func TestSortedMap(t *testing.T) {
+func TestSortedKeys(t *testing.T) {
 	sm := NewSortedMap()
 	sm.Set("a", 10)
 	sm.Set("c", 43)
 	sm.Set("d", 21)
 	t.Logf("%v", sm.SortedKeys())
+}
+
+func TestBasic(t *testing.T) {
+	sm := NewSortedMap()
+	sm.Set("foo", 34)
+	sm.Inc("foo", 3)
+	assert.Equal(t, 37, sm.Get("foo"))
+	sm.Inc("foo", -2)
+	assert.Equal(t, 35, sm.Get("foo"))
+	assert.Equal(t, -1, sm.Inc("non-exist", 5))
 }
