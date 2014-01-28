7c009dc29cae42754612133653c736e07b61e256
funkygao <funky.gao@gmail.com>
2014-01-28 09:20:47 +0800

    priority queue

---
+++ pq/priorityqueue.go
@@ -0,0 +1,58 @@
+package pq
+
+import (
+	"container/heap"
+)
+
+type Item struct {
+	Value    interface{}
+	Priority int
+
+	index int // The index of the item in the heap.
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue []*Item
+
+func New() *PriorityQueue {
+	return new(PriorityQueue)
+}
+
+func (this PriorityQueue) Len() int {
+	return len(this)
+}
+
+func (this PriorityQueue) Less(i, j int) bool {
+	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
+	return this[i].Priority > this[j].Priority
+}
+
+func (this PriorityQueue) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
+	this[i].index = i
+	this[j].index = j
+}
+
+func (this *PriorityQueue) Push(x interface{}) {
+	n := len(*this)
+	item := x.(*Item)
+	item.index = n
+	*this = append(*this, item)
+}
+
+func (this *PriorityQueue) Pop() interface{} {
+	old := *this
+	n := len(old)
+	item := old[n-1]
+	item.index = -1 // for safety
+	*this = old[0 : n-1]
+	return item
+}
+
+// update modifies the priority and value of an Item in the queue.
+func (this *PriorityQueue) update(item *Item, value interface{}, priority int) {
+	heap.Remove(this, item.index)
+	item.Value = value
+	item.Priority = priority
+	heap.Push(this, item)
+}
---
+++ pq/priorityqueue_test.go
@@ -0,0 +1,27 @@
+package pq
+
+import (
+	"container/heap"
+	"github.com/funkygao/assert"
+	"testing"
+)
+
+func TestPriorityQueue(t *testing.T) {
+	pq := New()
+	heap.Init(pq)
+	heap.Push(pq, &Item{Value: "hello3", Priority: 3})
+	heap.Push(pq, &Item{Value: "hello1", Priority: 1})
+	heap.Push(pq, &Item{Value: "hello8", Priority: 8})
+
+	item := heap.Pop(pq)
+	assert.Equal(t, "hello8", item.(*Item).Value.(string))
+
+	item = heap.Pop(pq)
+	assert.Equal(t, "hello3", item.(*Item).Value.(string))
+
+	item = heap.Pop(pq)
+	assert.Equal(t, "hello1", item.(*Item).Value.(string))
+
+	item = heap.Pop(pq)
+	assert.Equal(t, nil, item)
+}
