7cfaef252c7df2974bd40718cbeb5e7fcc41adab
funkygao <funky.gao@gmail.com>
2014-01-29 09:34:07 +0800

    bench

--- bjtime/time_test.go
+++ bjtime/time_test.go
@@ -13,3 +13,9 @@ func TestNowBj(t *testing.T) {
 func TestTsToString(t *testing.T) {
 	assert.Equal(t, "01-09 10:14:59", TsToString(1389233699))
 }
+
+func BenchmarkNowBj(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		NowBj()
+	}
+}
