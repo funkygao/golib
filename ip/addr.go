592e96337ad77ca6f0e63e71c2e81845722d030e
funkygao <funky.gao@gmail.com>
2014-02-03 16:37:50 +0800

    only get ipv4 address

--- ip/addr.go
+++ ip/addr.go
@@ -6,14 +6,21 @@ import (
 )
 
 // 127.0.0.1 excluded
-func LocalIpAddrs() (ips []string) {
+func LocalIpv4Addrs() (ips []string) {
 	info, _ := net.InterfaceAddrs()
 	ips = make([]string, 0)
 	for _, addr := range info {
 		ip := strings.Split(addr.String(), "/")[0]
-		if !strings.HasPrefix(ip, "127.0") {
-			ips = append(ips, ip)
+		if strings.HasPrefix(ip, "127.0") {
+			continue
 		}
+
+		trial := net.ParseIP(ip)
+		if trial.To4() == nil {
+			continue
+		}
+
+		ips = append(ips, ip)
 	}
 
 	return
