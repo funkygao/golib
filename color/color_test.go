3ea0f615e8fe16288ee9b70163bf77b4f3894782
funkygao <funky.gao@gmail.com>
2014-02-21 00:17:13 +0800

    more unit test

--- color/color_test.go
+++ color/color_test.go
@@ -9,3 +9,7 @@ func TestColorize(t *testing.T) {
 	assert.Equal(t, "\x1b[30m\x1b[41mhello\x1b[0m",
 		Colorize([]string{"FgBlack", "BgRed"}, "hello"))
 }
+
+func TestRed(t *testing.T) {
+	assert.Equal(t, "\x1b[31mhello\x1b[0m", Red("hello"))
+}
