907c52b950048bf0e625fd8a3ded908ca8ed0fea
funkygao <funky.gao@gmail.com>
2014-02-21 00:12:44 +0800

    handy funcs

--- color/color.go
+++ color/color.go
@@ -15,3 +15,31 @@ func Colorize(colors []string, str string) string {
 	r.WriteString(color_table[colorReset])
 	return r.String()
 }
+
+func Blue(str string) string {
+	return colorStr("FgBlue", str)
+}
+
+func Red(str string) string {
+	return colorStr("FgRed", str)
+}
+
+func Green(str string) string {
+	return colorStr("FgGreen", str)
+}
+
+func Yellow(str string) string {
+	return colorStr("FgYellow", str)
+}
+
+func Magenta(str string) string {
+	return colorStr("FgMagenta", str)
+}
+
+func Cyan(str string) string {
+	return colorStr("FgCyan", str)
+}
+
+func colorStr(color string, str string) string {
+	return Colorize([]string{color}, str)
+}
