d9edf9a40b3b312a8ab072688c61fd73611cd197
funkygao <funky.gao@gmail.com>
2014-11-12 09:58:44 +0800

    on init set random seed

--- sampling/sampling.go
+++ sampling/sampling.go
@@ -2,7 +2,12 @@ package sampling
 
 import (
 	"math/rand"
+    "time"
 )
+
+func init() {
+    rand.Seed(time.Now().UTC().UnixNano())
+}
 
 // Base is 1000
 func SampleRateSatisfied(rate int) bool {
