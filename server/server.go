6045ecb78dda52145afdd50761806843f474b014
funkygao <funky.gao@gmail.com>
2014-11-20 00:06:19 +0800

    util of killing a process by pidfile

--- server/server.go
+++ server/server.go
@@ -2,6 +2,10 @@ package server
 
 import (
 	conf "github.com/funkygao/jsconf"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"syscall"
 	"time"
 )
 
@@ -20,4 +24,27 @@ func NewServer(name string) (this *Server) {
 	this.Name = name
 
 	return
+}
+
+func KillProcess(pidFile string) error {
+	pidBody, err := ioutil.ReadFile(pidFile)
+	if err != nil {
+		return err
+	}
+
+	pid, err := strconv.Atoi(string(pidBody))
+	if err != nil {
+		return err
+	}
+
+	serverProcess, err := os.FindProcess(pid)
+	if err != nil {
+		return err
+	}
+
+	if err := serverProcess.Kill(); err != nil {
+		return err
+	}
+
+	return syscall.Unlink(pidFile)
 }
