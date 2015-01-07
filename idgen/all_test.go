eae68a046bb7df7f9dcdf82f70b988239c9f2bb7
funkygao <funky.gao@gmail.com>
2015-01-07 17:52:00 +0800

    next id with tag

--- idgen/all_test.go
+++ idgen/all_test.go
@@ -19,6 +19,30 @@ func TestNextId(t *testing.T) {
 
 }
 
+func TestNextIdWithTag(t *testing.T) {
+	id, _ := NewIdGenerator(1)
+	for i := 0; i < 100; i++ {
+		n, err := id.NextWithTag(5)
+		assert.Equal(t, nil, err)
+		n1, _ := id.NextWithTag(5)
+		if n1 <= n {
+			t.Errorf("expected %d > %d", n1, n)
+		}
+		t.Logf("n=%d, n1=%d", n, n1)
+	}
+
+}
+
+func TestNextIdWithTagError(t *testing.T) {
+	_, err := NewIdGenerator(32)
+	assert.Equal(t, ErrorInvalidWorkerId, err)
+	id, err := NewIdGenerator(31)
+	assert.Equal(t, nil, err)
+	val, err := id.NextWithTag(32)
+	assert.Equal(t, int64(0), val)
+	assert.Equal(t, ErrorInvalidTag, err)
+}
+
 func BenchmarkIdNext(b *testing.B) {
 	id, _ := NewIdGenerator(3)
 	for i := 0; i < b.N; i++ {
--- idgen/id.go
+++ idgen/id.go
@@ -9,14 +9,20 @@ import (
 var (
 	ErrorClockBackwards  = errors.New("Clock backwards")
 	ErrorInvalidWorkerId = errors.New("Too big worker id")
+	ErrorInvalidTag      = errors.New("Too big tag")
 )
 
 const (
-	WorkerIdBits       = uint64(5) // max 31
-	SequenceBits       = uint64(12)
+	WorkerIdBits       = uint64(5)  // max 31
+	TagIdBits          = uint64(5)  // max 31
+	SequenceBits       = uint64(12) // max 4095, limit 3M/s
 	WorkerIdShift      = SequenceBits
-	TimestampLeftShift = SequenceBits + WorkerIdBits
+	TagIdShift         = SequenceBits + WorkerIdBits
+	TimestampLeftShift = SequenceBits + WorkerIdBits + TagIdBits
 	SequenceMask       = int64(-1) ^ (int64(-1) << SequenceBits)
+
+	MaxTagId    = (1 << TagIdBits) - 1
+	MaxWorkerId = (1 << WorkerIdBits) - 1
 
 	// Sat Jan  4 19:29:34 2014
 	twepoch = int64(1388834974657)
@@ -34,7 +40,7 @@ type IdGenerator struct {
 func NewIdGenerator(wid int) (this *IdGenerator, err error) {
 	this = new(IdGenerator)
 	this.wid = int64(wid)
-	if wid > (1<<WorkerIdBits)-1 {
+	if wid > MaxWorkerId {
 		return nil, ErrorInvalidWorkerId
 	}
 	return
@@ -45,6 +51,18 @@ func (this *IdGenerator) milliseconds() int64 {
 }
 
 func (this *IdGenerator) Next() (int64, error) {
+	return this.nextId(0)
+}
+
+func (this *IdGenerator) NextWithTag(tag int16) (int64, error) {
+	return this.nextId(tag)
+}
+
+func (this *IdGenerator) nextId(tag int16) (int64, error) {
+	if tag > MaxTagId {
+		return 0, ErrorInvalidTag
+	}
+
 	this.mutex.Lock()
 	defer this.mutex.Unlock()
 
@@ -68,6 +86,7 @@ func (this *IdGenerator) Next() (int64, error) {
 
 	r := ((ts - twepoch) << TimestampLeftShift) |
 		(this.wid << WorkerIdShift) |
+		(int64(tag) << TagIdShift) |
 		this.seq
 	return r, nil
 }
