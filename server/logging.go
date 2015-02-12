7ccd32f97a298b1bd8c867990b495530a6f15cb8
funkygao <funky.gao@gmail.com>
2015-02-12 23:01:51 +0800

    integration with alarm log, prepare for dpipe

--- server/logging.go
+++ server/logging.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-func SetupLogging(logFile, logLevel, crashLogFile string) {
+func SetupLogging(logFile, logLevel, crashLogFile, alarmSockPath, alarmTag string) {
 	level := log.DEBUG
 	switch logLevel {
 	case "info":
@@ -25,23 +25,36 @@ func SetupLogging(logFile, logLevel, crashLogFile string) {
 
 	case "trace":
 		level = log.TRACE
+
+	case "alarm":
+		level = log.ALARM
 	}
 
 	for _, filter := range log.Global {
 		filter.Level = level
 	}
 
+	// TODO
+	log.LogBufferLength = 2 << 10 // default 32, chan cap
+
 	if logFile == "stdout" {
 		log.AddFilter("stdout", level, log.NewConsoleLogWriter())
 	} else {
-		writer := log.NewFileLogWriter(logFile, false)
 		log.DeleteFilter("stdout")
-		log.AddFilter("file", level, writer)
-		writer.SetFormat("[%d %T] [%L] (%S) %M")
-		writer.SetRotate(true)
-		writer.SetRotateSize(0)
-		writer.SetRotateLines(0)
-		writer.SetRotateDaily(true)
+
+		filer := log.NewFileLogWriter(logFile, false)
+		filer.SetFormat("[%d %T] [%L] (%S) %M")
+		filer.SetRotate(true)
+		filer.SetRotateSize(0)
+		filer.SetRotateLines(0)
+		filer.SetRotateDaily(true)
+		log.AddFilter("file", level, filer)
+
+		if alarmer, err := log.NewSyslogNgWriter(alarmSockPath, alarmTag); err != nil {
+			log.Error("syslogng writer: %s", err.Error())
+		} else {
+			log.AddFilter("alarm", log.ALARM, alarmer)
+		}
 	}
 
 	if crashLogFile != "" {
