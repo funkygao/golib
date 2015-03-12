83c680318f51c3aad1b33feda072a5fdeb4bd8d9
funkygao <funky.gao@gmail.com>
2015-03-12 12:03:01 +0800

    more benchmark for lru cache

--- cache/bench_test.go
+++ cache/bench_test.go
@@ -4,18 +4,30 @@ import (
 	"fmt"
 	"hash/adler32"
 	"hash/crc32"
+	"hash/fnv"
 	"math/rand"
 	"strings"
 	"testing"
 )
 
-func BenchmarkCreateKey(b *testing.B) {
+// 4.5 ns/op
+func BenchmarkGolangTypeAssert(b *testing.B) {
+	var a interface{} = 45
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		if _, ok := a.(int); ok {
+
+		}
+	}
+}
+
+func BenchmarkCreateRandKey(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		fmt.Sprintf("mc_stress:%d", rand.Int())
 	}
 }
 
-func BenchmarkSet(b *testing.B) {
+func BenchmarkSetWithRandKey(b *testing.B) {
 	b.ReportAllocs()
 	lru := NewLruCache(1 << 20)
 	var key string
@@ -26,13 +38,34 @@ func BenchmarkSet(b *testing.B) {
 	b.SetBytes(int64(len(key)))
 }
 
-func BenchmarkGet(b *testing.B) {
+func BenchmarkGetWithRandKey(b *testing.B) {
 	b.ReportAllocs()
 	lru := NewLruCache(1 << 10)
 	var key string
 	for i := 0; i < b.N; i++ {
 		key = fmt.Sprintf("mc_stress:%d", rand.Int())
 		lru.Set(key, 5)
+		lru.Get(key)
+	}
+	b.SetBytes(int64(len(key)))
+}
+
+func BenchmarkSetWithSameKey(b *testing.B) {
+	b.ReportAllocs()
+	lru := NewLruCache(1 << 10)
+	var key string = "stuff"
+	for i := 0; i < b.N; i++ {
+		lru.Set(key, 1000)
+	}
+	b.SetBytes(int64(len(key)))
+}
+
+func BenchmarkGetWithSameKey(b *testing.B) {
+	b.ReportAllocs()
+	lru := NewLruCache(1 << 10)
+	var key string = "stuff"
+	lru.Set(key, 1000)
+	for i := 0; i < b.N; i++ {
 		lru.Get(key)
 	}
 	b.SetBytes(int64(len(key)))
@@ -52,6 +85,17 @@ func BenchmarkAdler32Of100B(b *testing.B) {
 	key := strings.Repeat("s", 100)
 	for i := 0; i < b.N; i++ {
 		adler32.Checksum([]byte(key))
+	}
+	b.SetBytes(100)
+}
+
+func BenchmarkFnv100B(b *testing.B) {
+	b.ReportAllocs()
+	key := strings.Repeat("s", 100)
+	f := fnv.New32()
+	for i := 0; i < b.N; i++ {
+		f.Write([]byte(key))
+		f.Sum32()
 	}
 	b.SetBytes(100)
 }
