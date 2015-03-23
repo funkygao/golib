afb70f1eea45d4d89ec1e7b0ceb6388530f160d6
funkygao <funky.gao@gmail.com>
2015-03-23 15:47:56 +0800

    bench compare with strconv

--- conv/bench_test.go
+++ conv/bench_test.go
@@ -1,12 +1,22 @@
 package conv
 
 import (
+	"strconv"
 	"testing"
 )
 
+var v = "12232"
+
 func BenchmarkParseInt(b *testing.B) {
-	s := []byte("12232")
+	b.ReportAllocs()
+	s := []byte(v)
 	for i := 0; i < b.N; i++ {
 		ParseInt(s)
+	}
+}
+
+func BenchmarkStrconvParseInt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		strconv.ParseInt(v, 10, 64)
 	}
 }
