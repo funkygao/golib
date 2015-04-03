800cda2d9ce899738f02ecf7df58d9a3f7057f84
funkygao <funky.gao@gmail.com>
2015-04-03 10:43:52 +0800

    add io pkg

---
+++ io/read.go
@@ -0,0 +1,22 @@
+package syslogng
+
+import (
+	"bufio"
+)
+
+// ReadLine is a helper func for bufio's ReadLine that
+// read the complete line no matter how long it is.
+func ReadLine(bio *bufio.Reader) ([]byte, error) {
+	line, isPrefix, err := bio.ReadLine()
+	if !isPrefix {
+		return line, err
+	}
+
+	// line is too long, read till eol
+	buf := append([]byte(nil), line...)
+	for isPrefix && err == nil {
+		line, isPrefix, err = bio.ReadLine()
+		buf = append(buf, line...)
+	}
+	return buf, err
+}
