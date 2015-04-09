2d0d97e4222f7e18c4fe641d624db033a82de906
funkygao <funky.gao@gmail.com>
2015-04-09 21:45:42 +0800

    fix dashboard pkg doc fmt problem

--- dashboard/doc.go
+++ dashboard/doc.go
@@ -1,13 +1,12 @@
-// Package dashboard implements a visualization feature that
-// listen on a specified http port and draws stacked trend lines over time.
-//
-// Usage:
-//    d := dashboard.New("gc", 10)
-//    if err := d.Validate(); err != nil {
-//	      panic(err)
-//	  }
-//	  gcGraph := dashboard.NewGraph("gc")
-//    gcGraph.AddLine("Heap", myline{i: 1})
-//    d.AddGraph(gcGraph)
-//	  d.Launch(":8000")
+/*
+Package dashboard helps for visualization that listen on a
+http port and draws stacked trend lines over time.
+
+Usage:
+	d := dashboard.New("test of dashboard", 5)
+	g := d.AddGraph("graph1")
+	g.AddLine("Heap", &mydata{i: 1})
+	d.Launch(":8000")
+
+*/
 package dashboard
