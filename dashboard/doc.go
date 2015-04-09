9e641d0d8b11267c638007c28347499ec9a8c3d5
funkygao <funky.gao@gmail.com>
2015-04-09 22:29:26 +0800

    tweak of dashboard godoc

--- dashboard/doc.go
+++ dashboard/doc.go
@@ -5,7 +5,8 @@ http port and draws stacked trend lines over time.
 Usage:
 	d := dashboard.New("test of dashboard", 5)
 	g := d.AddGraph("graph1")
-	g.AddLine("Heap", &mydata{i: 1})
+	g.AddLine("data1", &mydata{i: 1})
+	g.AddLine("data2", &mydata2{i: 0})
 	d.Launch(":8000")
 
 */
