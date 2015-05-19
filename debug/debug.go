04c35e87fd82d2bb27062f90266a616fdb837a97
funkygao <funky.gao@gmail.com>
2015-05-19 08:06:16 +0800

    debugf

---
+++ debug/debug.go
@@ -0,0 +1,44 @@
+package debug
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/funkygao/golib/color"
+)
+
+var (
+	Debug     = false
+	debugLock sync.Mutex
+)
+
+func Debugf(format string, args ...interface{}) {
+	if !Debug {
+		return
+	}
+
+	pc, file, line, ok := runtime.Caller(1)
+	if !ok {
+		file = "<?>"
+		line = 0
+	} else {
+		if i := strings.LastIndex(file, "/"); i >= 0 {
+			file = file[i+1:]
+		}
+	}
+	fn := runtime.FuncForPC(pc).Name()
+	fnparts := strings.Split(fn, "/")
+	t := time.Now()
+	hour, min, sec := t.Clock()
+	nanosec := t.Nanosecond() / 1e3
+
+	debugLock.Lock()
+	fmt.Printf("DEBUG: [%d:%d:%d.%04d] %s:%d(%s): %s\n",
+		hour, min, sec, nanosec,
+		file, line, color.Red(fnparts[len(fnparts)-1]),
+		fmt.Sprintf(format, args...))
+	debugLock.Unlock()
+}
