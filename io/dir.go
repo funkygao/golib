81a5bbee33467ca4fcaa2ccf79a67be6753891c4
funkygao <funky.gao@gmail.com>
2015-12-19 13:48:35 +0800

    add a handy func: DirChildren

--- io/dir.go
+++ io/dir.go
@@ -2,6 +2,7 @@ package io
 
 import (
 	"os"
+	"path/filepath"
 )
 
 func DirExists(path string) bool {
@@ -12,4 +13,16 @@ func DirExists(path string) bool {
 		return fi.IsDir()
 	}
 	return false
+}
+
+func DirChildren(root string, predicate func(string, os.FileInfo, error) bool) []string {
+	r := make([]string, 0)
+	walkFn := func(path string, info os.FileInfo, err error) error {
+		if predicate(path, info, err) {
+			r = append(r, path)
+		}
+		return nil
+	}
+	filepath.Walk(root, walkFn)
+	return r
 }
--- io/dir_test.go
+++ io/dir_test.go
@@ -1,6 +1,7 @@
 package io
 
 import (
+	"os"
 	"testing"
 
 	"github.com/funkygao/assert"
@@ -9,4 +10,19 @@ import (
 func TestDirExists(t *testing.T) {
 	assert.Equal(t, true, DirExists("/tmp"))
 	assert.Equal(t, false, DirExists("/tmp____xxxx"))
+}
+
+func TestDirChildren(t *testing.T) {
+	predicate := func(path string, info os.FileInfo, err error) bool {
+		if err != nil {
+			return false
+		}
+		if info.IsDir() {
+			return false
+		}
+
+		return true
+	}
+	children := DirChildren(".", predicate)
+	t.Logf("%+v", children)
 }
