738546491fb04451b74de020fd7b7d768af4d7be
funkygao <funky.gao@gmail.com>
2016-06-29 22:08:13 +0800

    WaitTimeout feature added

--- wg/wg.go
+++ wg/wg.go
@@ -2,6 +2,7 @@ package wg
 
 import (
 	"sync"
+	"time"
 )
 
 type WaitGroupWrapper struct {
@@ -18,4 +19,22 @@ func (this *WaitGroupWrapper) Wrap(cb func()) {
 
 func (this *WaitGroupWrapper) Wait() {
 	this.wg.Wait()
+}
+
+// WaitTimeout is same as Wait except that it accepts timeout arguement.
+// FIXME if timeout triggered, there will be goroutine leak.
+func (this *WaitGroupWrapper) WaitTimeout(timeout time.Duration) bool {
+	c := make(chan struct{})
+	go func() {
+		defer close(c)
+		this.wg.Wait()
+	}()
+
+	select {
+	case <-c:
+		return false // completed normally
+
+	case <-time.After(timeout):
+		return true // timed out
+	}
 }
