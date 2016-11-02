76a9f5aaa5e526553d17943d79c3f2b9c69d1821
Funky Gao <funky.gao@gmail.com>
2016-11-02 09:48:18 +0800

    default 5s tick

--- stress/options.go
+++ stress/options.go
@@ -25,7 +25,7 @@ func init() {
 	flag.IntVar(&Flags.C1, "c1", 1, "Number of low concurrency")
 	flag.IntVar(&Flags.C2, "c2", 1000, "Number of high concurrency")
 	flag.IntVar(&Flags.Step, "step", 20, "Concurrency step between each round")
-	flag.Int64Var(&Flags.Tick, "tick", 2, "Console stats runner ticker in seconds")
+	flag.Int64Var(&Flags.Tick, "tick", 5, "Console stats runner ticker in seconds")
 	flag.BoolVar(&Flags.Neat, "neat", false, "Display in neat mode, with less output")
 	flag.StringVar(&Flags.MasterAddr, "master", "", "Master stress address. If empty, works as master")
 }
