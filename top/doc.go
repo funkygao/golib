34308193bb5c49e0cef80d06eb5ea403b7edb034
Funky Gao <funky.gao@gmail.com>
2017-01-06 13:07:11 +0800

    an example given

--- top/doc.go
+++ top/doc.go
@@ -1,2 +1,39 @@
 // Package top provides unix top like UI framework.
+//
+//
+/*
+Usage example:
+
+	package main
+
+	import (
+		"fmt"
+		"time"
+
+		"github.com/funkygao/golib/top"
+	)
+
+	func main() {
+		t := top.New("Id|Value", "%2s %8s")
+		go func() {
+			for i := 0; i < 1000; i++ {
+				rows := make([]string, 0)
+
+				if i%2 == 0 {
+					rows = append(rows, fmt.Sprintf("%d|%d", 1, 100))
+				} else {
+					rows = append(rows, fmt.Sprintf("%d|%d", 2, 200))
+				}
+
+				t.Refresh(rows)
+				time.Sleep(time.Second)
+			}
+		}()
+
+		if err := t.Start(); err != nil {
+			panic(err)
+		}
+	}
+
+*/
 package top
