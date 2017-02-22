2d506b3502a04ad8af2df7000bc8bb029fe09d4d
Funky Gao <funky.gao@gmail.com>
2017-02-22 13:38:33 +0800

    refactor ring buffer pkg

--- ringbuffer/rb.go
+++ ringbuffer/rb.go
@@ -1,60 +1,61 @@
 package ringbuffer
 
-import "fmt"
-import "sync/atomic"
-import "runtime"
-
-const (
-	queueSize uint64 = 4096
-	indexMask uint64 = queueSize - 1
+import (
+	"runtime"
+	"sync/atomic"
 )
 
-type ringBuffer struct {
-	padding1           [8]uint64
-	lastCommittedIndex uint64
+type RingBuffer struct {
+	queueSize uint64
+	indexMask uint64
+
+	padding1           [6]uint64
+	lastCommittedIndex uint64 //
 	padding2           [8]uint64
-	nextFreeIndex      uint64
+	nextFreeIndex      uint64 //
 	padding3           [8]uint64
-	readerIndex        uint64
+	readerIndex        uint64 //
 	padding4           [8]uint64
-	contents           [queueSize]interface{}
-	padding5           [8]uint64
+	contents           []interface{}
 }
 
-func New() *ringBuffer {
-	return &ringBuffer{lastCommittedIndex: 0, nextFreeIndex: 1, readerIndex: 1}
+// New creates a ring buffer.
+func New(queueSize uint64) (*RingBuffer, error) {
+	if queueSize == 1 || queueSize&(queueSize-1) != 0 {
+		return nil, ErrInvalidQueueSize
+	}
+
+	return &RingBuffer{
+		queueSize:          queueSize,
+		indexMask:          queueSize - 1,
+		lastCommittedIndex: 0,
+		nextFreeIndex:      1,
+		readerIndex:        1,
+		contents:           make([]interface{}, queueSize),
+	}, nil
 }
 
-func (this *ringBuffer) Write(value interface{}) {
-	var myIndex = atomic.AddUint64(&this.nextFreeIndex, 1) - 1
+func (rb *RingBuffer) Write(value interface{}) {
+	var myIndex = atomic.AddUint64(&rb.nextFreeIndex, 1) - 1
 	// Wait for reader to catch up, so we don't clobber a slot which it is (or will be) reading
-	for myIndex > (this.readerIndex + queueSize - 2) {
+	for myIndex > (rb.readerIndex + rb.queueSize - 2) {
 		runtime.Gosched()
 	}
 
 	// Write the item into it's slot
-	this.contents[myIndex&indexMask] = value
+	rb.contents[myIndex&rb.indexMask] = value
 	// Increment the lastCommittedIndex so the item is available for reading
-	for !atomic.CompareAndSwapUint64(&this.lastCommittedIndex, myIndex-1, myIndex) {
+	for !atomic.CompareAndSwapUint64(&rb.lastCommittedIndex, myIndex-1, myIndex) {
 		runtime.Gosched()
 	}
 }
 
-func (this *ringBuffer) Read() interface{} {
-	var myIndex = atomic.AddUint64(&this.readerIndex, 1) - 1
+func (rb *RingBuffer) Read() interface{} {
+	var myIndex = atomic.AddUint64(&rb.readerIndex, 1) - 1
 	// If reader has out-run writer, wait for a value to be committed
-	for myIndex > this.lastCommittedIndex {
+	for myIndex > rb.lastCommittedIndex {
 		runtime.Gosched()
 	}
 
-	return this.contents[myIndex&indexMask]
-}
-
-func (this *ringBuffer) PrintDump() {
-	fmt.Printf("lastCommitted: %3d, nextFree: %3d, readerIndex: %3d, content:",
-		this.lastCommittedIndex, this.nextFreeIndex, this.readerIndex)
-	for idx, value := range this.contents {
-		fmt.Printf("%5v : %5v", idx, value)
-	}
-	fmt.Print("\n")
+	return rb.contents[myIndex&rb.indexMask]
 }
--- ringbuffer/rb_test.go
+++ ringbuffer/rb_test.go
@@ -1,22 +1,55 @@
 package ringbuffer
 
 import (
-	"github.com/funkygao/assert"
 	"testing"
+
+	"github.com/funkygao/assert"
 )
 
-func TestRingBuffer(t *testing.T) {
-	rb := New()
+func TestRingBufferBasics(t *testing.T) {
+	rb, _ := New(16)
 	rb.Write("hello")
 	rb.Write(189)
 	v1 := rb.Read().(string)
 	assert.Equal(t, "hello", v1)
 	v2 := rb.Read().(int)
 	assert.Equal(t, 189, v2)
+	if testing.Verbose() {
+		t.Logf("%+v", rb)
+	}
+}
+
+func TestRingBufferRewind(t *testing.T) {
+	rb, _ := New(128)
+	for i := 0; i < 1<<20; i++ {
+		rb.Write(i)
+		assert.Equal(t, i, rb.Read().(int))
+	}
+	if testing.Verbose() {
+		t.Logf("%+v", rb)
+	}
+}
+
+func TestRinbBufferAdvanceAndRewind(t *testing.T) {
+
+}
+
+func TestNewWithError(t *testing.T) {
+	for _, sz := range []uint64{1, 3, 5, 10, 12, 28, 4098} {
+		_, err := New(sz)
+		if err != ErrInvalidQueueSize {
+			t.Fatal("should return err")
+		}
+	}
+
+	_, err := New(2 << 12)
+	if err != nil {
+		t.Fatal("should not return err")
+	}
 }
 
 func BenchmarkRingBuffer(b *testing.B) {
-	rb := New()
+	rb, _ := New(4096)
 	data := "value"
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
